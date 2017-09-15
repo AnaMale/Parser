42b7327bad230b71e9e2cadc274113170a37e3de
AnaMale <malesevicana@gmail.com>
2017-09-15 15:52:21 +0200

    Add files via upload

---
+++ sept/workspace/MJCompiler/src/rs/ac/bg/etf/pp1/MyTab.java
@@ -0,0 +1,17 @@
+package rs.ac.bg.etf.pp1;
+
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
+
+public class MyTab extends Tab {
+
+	 public static Struct boolType = new Struct(Struct.None);
+	
+	 
+	 public static void init(){
+		 Tab.init();
+		 Tab.currentScope().addToLocals(new Obj(Obj.Type, "bool", boolType));
+	 }
+	
+}
